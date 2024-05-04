098fcf090d7f50ff68dba782ab244c7ec3d2bb43
blake-lim <blake198492@kakao.com>
2024-05-04 13:24:06 +0900

    ENH : Added MenuScreen

---
+++ lib/views/menu_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Animations'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Animated Container'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
