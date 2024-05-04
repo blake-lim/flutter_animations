2eef0cd053aa159398b9c478118492185dc42010
blake-lim <blake198492@kakao.com>
2024-05-04 13:32:06 +0900

    ENH : 자주 사용되는 내비게이션 로직 함수로 변환

--- lib/views/menu_screen.dart
+++ lib/views/menu_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/views/implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
+
+  void _goToPage(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => screen,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +23,9 @@ class MenuScreen extends StatelessWidget {
           child: Column(
             children: [
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _goToPage(context, const ImplicitAnimationScreen());
+                },
                 child: const Text('Animated Container'),
               ),
             ],
